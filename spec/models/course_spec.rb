d4942df0246aaeba022508ec0786792cf5ef86a6
JonathanTR <Jonathan.d.reilly@gmail.com>
2014-01-28 16:23:44 -0700

    courses have many seminars

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -4,4 +4,5 @@ describe Course do
   it { should validate_presence_of :title}
   it { should validate_uniqueness_of :title}
   it { should have_and_belong_to_many(:locations) }
+  it { should have_many(:seminars) }
 end
