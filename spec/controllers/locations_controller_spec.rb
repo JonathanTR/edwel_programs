5a9b6fc826d5f1486d6cb700af5e7dbe0f3cca3c
JonathanTR <Jonathan.d.reilly@gmail.com>
2014-01-29 11:32:27 -0700

    Add show route, action, view to location

--- spec/controllers/locations_controller_spec.rb
+++ spec/controllers/locations_controller_spec.rb
@@ -1,8 +1,14 @@
 require 'spec_helper'
 
 describe LocationsController do
-  it 'should have a valid #index' do
+  it 'should have a valid #index route' do
     get :index
+    response.status.should eq(200)
+  end
+
+  it 'should have a valid #show route' do
+    chicago = Location.create(name: 'Chicago')
+    get :show, id: chicago.id
     response.status.should eq(200)
   end
 end
