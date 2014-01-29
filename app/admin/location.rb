8ca70fc9fd59075f3e1c9d6febd607303104ce77
JonathanTR <Jonathan.d.reilly@gmail.com>
2014-01-29 14:47:16 -0700

    add slugs to locations. include slug history and redirect

--- app/admin/location.rb
+++ app/admin/location.rb
@@ -5,6 +5,12 @@ ActiveAdmin.register Location do
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   permit_params :id, :name, :slug, {course_ids: []}
+
+  controller do
+    def find_resource
+      scoped_collection.friendly.find(params[:id])
+    end
+  end
   
   index do
     column :id
--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -4,6 +4,10 @@ class LocationsController < ApplicationController
   end
 
   def show
+    @location = Location.friendly.find(params[:id])
+    if request.path != location_path(@location)
+      return redirect_to @location, :status => :moved_permanently
+    end
   end
 
 end
