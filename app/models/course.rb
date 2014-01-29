89e0bb7cb72e6ca340cedb5474ac31dfe051295b
JonathanTR <Jonathan.d.reilly@gmail.com>
2014-01-29 16:25:21 -0700

    add friendly id slugs to courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,7 @@
 class Course < ActiveRecord::Base
+  extend FriendlyId
+  friendly_id :title, use: [:slugged, :history]
+
   validates_presence_of :title
   validates_uniqueness_of :title
   has_and_belongs_to_many :locations
