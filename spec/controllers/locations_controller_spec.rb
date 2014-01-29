cccbb4876fafcbf28c80579616142102b0384444
JonathanTR <Jonathan.d.reilly@gmail.com>
2014-01-29 15:25:19 -0700

    update location controller test to work with friendly id

--- spec/controllers/locations_controller_spec.rb
+++ spec/controllers/locations_controller_spec.rb
@@ -8,7 +8,7 @@ describe LocationsController do
 
   it 'should have a valid #show route' do
     chicago = Location.create(name: 'Chicago')
-    get :show, id: chicago.id
+    get :show, id: chicago.slug
     response.status.should eq(200)
   end
 end
