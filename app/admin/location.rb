938c5c99c04ded1b07be26cf6f7e4bb74e6f1fbf
JonathanTR <Jonathan.d.reilly@gmail.com>
2014-01-29 12:17:57 -0700

    add friendly_id slug to locations

--- app/admin/location.rb
+++ app/admin/location.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register Location do
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
-  permit_params :id, :name, {course_ids: []}
+  permit_params :id, :name, :slug, {course_ids: []}
   
   index do
     column :id
