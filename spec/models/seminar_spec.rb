87c6bc5ce366c7c51abdd1b05d7c5ada81c624b6
JonathanTR <Jonathan.d.reilly@gmail.com>
2014-01-29 08:42:37 -0700

    Additional test for seminar course relationship

--- spec/models/seminar_spec.rb
+++ spec/models/seminar_spec.rb
@@ -3,4 +3,11 @@ require 'spec_helper'
 describe Seminar do
   it { should belong_to(:course) }
   it { should belong_to(:location) }
+
+  it 'should be able to add a course' do
+    pmp = Course.create(title: 'PMP')
+    atlanta_feb_pmp = Seminar.create(name: 'Atlanta February PMP')
+    atlanta_feb_pmp.course = pmp
+    expect(atlanta_feb_pmp.course).to eq(pmp)
+  end
 end
