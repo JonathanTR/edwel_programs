34e63d1485a55dbf9e7e13b0a99c1be553836ab1
JonathanTR <Jonathan.d.reilly@gmail.com>
2014-01-29 15:25:19 -0700

    update location controller test to work with friendly id

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 EdwelPrograms::Application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  
-  resources :locations, only: [:index, :show]
 
   root to: 'locations#index'
+  
+  resources :locations, only: [:index, :show], path: ''
+
 
 end
