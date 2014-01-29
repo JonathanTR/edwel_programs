d894d6d20d68fb410703de591170ad4057150887
JonathanTR <Jonathan.d.reilly@gmail.com>
2014-01-29 11:32:27 -0700

    Add show route, action, view to location

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -3,4 +3,7 @@ class LocationsController < ApplicationController
   def index
   end
 
+  def show
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ EdwelPrograms::Application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   
-  resources :locations, only: [:index]
+  resources :locations, only: [:index, :show]
 
   root to: 'locations#index'
 
