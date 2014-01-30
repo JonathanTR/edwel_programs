c520f55b192291199955f2a7370673023f07920e
JonathanTR <Jonathan.d.reilly@gmail.com>
2014-01-29 17:24:42 -0700

    add preliminary views

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -3,6 +3,7 @@ class CoursesController < ApplicationController
   def show
     @course = Course.friendly.find(params[:id])
     @location = Location.friendly.find(params[:location_id])
+    @seminars = Seminar.where(course: @course, location: @location)
 
     if request.path != location_course_path(@location, @course)
       return redirect_to location_course_path(@location, @course), :status => :moved_permanently
