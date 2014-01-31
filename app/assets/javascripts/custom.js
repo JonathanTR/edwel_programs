e0a8cec9d762d260f196a917f0c6c719241a7b0a
JonathanTR <Jonathan.d.reilly@gmail.com>
2014-01-30 19:34:21 -0700

    use no-flexboxlegacy to signal old ie

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  if(!Modernizr.flexbox){
+  if(!Modernizr.flexboxlegacy){
     $('.navbar-toggle').remove()
     $('.navbar-header').append('<button type="button" class="btn btn-success navbar-btn navbar-right" id="register">Register</button>')
   }
