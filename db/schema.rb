0db9960c86271d9d60749789baef2f232c825c33
JonathanTR <Jonathan.d.reilly@gmail.com>
2014-02-05 15:44:02 -0700

    integrate active-admin wsyihtml5 editor

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140204195053) do
+ActiveRecord::Schema.define(version: 20140205224054) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
