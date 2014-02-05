8b863c7413ae3dd40f6598e679311a073161cfec
JonathanTR <Jonathan.d.reilly@gmail.com>
2014-02-05 14:54:44 -0700

    Use Modernizr.mediaqueries to detect navbar problems in ie7
    * this is more accurate to the actual problem

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  if(!Modernizr.flexboxlegacy){
+  if(!Modernizr.mediaqueries){
     $('.navbar-toggle').remove()
     $('.navbar-header').append('<button type="button" class="btn btn-success navbar-btn navbar-right" id="register">Register</button>')
   }
