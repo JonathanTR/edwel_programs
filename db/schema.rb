04eb79a8e0956ffac982a686fe2fd13929d7b559
JonathanTR <Jonathan.d.reilly@gmail.com>
2014-02-06 00:23:47 -0700

    add approach to course model and html editor for approach in admin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140206000626) do
+ActiveRecord::Schema.define(version: 20140206072152) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 20140206000626) do
     t.text     "content"
     t.string   "tagline"
     t.text     "description"
+    t.text     "approach"
   end
 
   add_index "courses", ["slug"], name: "index_courses_on_slug", unique: true, using: :btree
