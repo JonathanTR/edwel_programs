5936d8a24bee9d0796ab1a1af4ee333d88c49032
JonathanTR <Jonathan.d.reilly@gmail.com>
2014-02-10 11:46:49 -0700

    fit sections to window

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,6 +1,12 @@
+fitSectionsToWindow = function(){
+  $('section').height($(window).height() * 0.9)
+}
+
 $(document).ready(function(){
   if(!Modernizr.mediaqueries){
     $('.navbar-toggle').remove()
     $('.navbar-header').append('<button type="button" class="btn btn-success navbar-btn navbar-right" id="register">Register</button>')
   }
+
+  fitSectionsToWindow()
 })
