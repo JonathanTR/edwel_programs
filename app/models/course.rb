b003578dcb090fd35feef587dbc29828e9899648
JonathanTR <Jonathan.d.reilly@gmail.com>
2014-02-13 15:11:21 -0700

    Courses accept nested attributes for content_blocks

--- app/models/course.rb
+++ app/models/course.rb
@@ -4,7 +4,10 @@ class Course < ActiveRecord::Base
 
   validates_presence_of :title
   validates_uniqueness_of :title
+  
   has_and_belongs_to_many :locations
   has_many :seminars
   has_many :content_blocks
+
+  accepts_nested_attributes_for :content_blocks
 end
