0738b126173c9d30a67f7f5a87e677ff3d12b4f2
JonathanTR <Jonathan.d.reilly@gmail.com>
2014-02-13 11:40:04 -0700

    Fix for turbolinks loading issue
    - apparently jQuery's 'ready' method doesn't fire when using turbolinks. This is because all of the javascript is loaded up right at the beginning and content is the only thing changed subsequently. The fix here is to use turbolinks' page:load event instead.

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,14 +1,21 @@
-fitSectionsToWindow = function(){
+var fitSectionsToWindow = function(){
   if ($('section').height() < ($(window).height() * 0.9)){
     $('section').height($(window).height() * 0.9)
   }
 }
 
-$(document).ready(function(){
+var tempNavbarFix = function(){
   if(!Modernizr.mediaqueries){
     $('.navbar-toggle').remove()
     $('.navbar-header').append('<button type="button" class="btn btn-success navbar-btn navbar-right" id="register">Register</button>')
   }
+}
 
+var ready = function(){
+  tempNavbarFix()
   fitSectionsToWindow()
-})
+}
+
+
+$(document).ready(ready)
+$(document).on('page:load', ready)
