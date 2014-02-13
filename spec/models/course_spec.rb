244f316e04565a396a81fe8a946ef1fa3a3c6889
JonathanTR <Jonathan.d.reilly@gmail.com>
2014-02-13 15:11:21 -0700

    Courses accept nested attributes for content_blocks

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -6,4 +6,5 @@ describe Course do
   it { should have_and_belong_to_many(:locations) }
   it { should have_many(:seminars) }
   it { should have_many(:content_blocks) }
+  it { should accept_nested_attributes_for :content_blocks}
 end
