1e673f95769622925dcf8e03f72ba01d7f8edb03
JonathanTR <Jonathan.d.reilly@gmail.com>
2014-02-24 11:37:32 -0700

    mailchimp controller renders messages directly to JSON

--- app/controllers/mailchimp_controller.rb
+++ app/controllers/mailchimp_controller.rb
@@ -13,20 +13,20 @@ class MailchimpController < ApplicationController
         @mailchimp.lists.subscribe(@list_id, {'email' => email}, {'fname' => fname, 'lname' => lname, 'phone' => phone, 'company' => company}, 'html', false)
         render json: { message: "Thank you for registering. A representative will be in touch with shortly to answer your questions."}.to_json
       rescue Mailchimp::ListAlreadySubscribedError 
-        format.json{render :json => {:message => "#{email} is already subscribed to the list"}}
+        render json: {:message => "#{email} is already subscribed to the list"}.to_json
       rescue Mailchimp::ListMergeFieldRequiredError
-        format.json{render :json => {:message => "Please fill out all the required fields"}}
+        render json: {:message => "Please fill out all the required fields"}.to_json
       rescue Mailchimp::Error => ex
         if ex.message
-          puts '*' * 800
+          puts '*' * 100
           puts ex
-          format.json{render :json => {:message => "An unknown error occurred."}}
+          render json: {:message => "An unknown error occurred."}.to_json
         else
-          format.json{render :json => {:message => "An unknown error occurred."}}
+          render json: {:message => "An unknown error occurred."}.to_json
         end
       end
     else
-      format.json{render :json => {:message => "Email Address Cannot be blank. Please enter valid email id."}}
+      render json: {:message => "Email Address Cannot be blank. Please enter valid email id."}.to_json
     end
   end
 
