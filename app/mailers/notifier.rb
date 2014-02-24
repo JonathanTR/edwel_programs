bba7995dae6a0ce7a8b2702130403002ab0ec12a
JonathanTR <Jonathan.d.reilly@gmail.com>
2014-02-24 02:04:49 -0700

    properly capitalize all names and company names

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -2,11 +2,11 @@ class Notifier < ActionMailer::Base
   default from: "jonathan.d.reilly@gmail.com"
 
   def registration_request(fname, lname, email, phone, company, message)
-    @fname = fname
-    @lname = lname
+    @fname = fname.capitalize
+    @lname = lname.capitalize
     @email = email
     @phone = phone
-    @company = company
+    @company = company.capitalize
     @message = message
     mail(to: "jonathan.d.reilly@gmail.com", subject: "New registration")
   end
